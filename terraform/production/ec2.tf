d1a80df9237bacd63b18f444b724a287e76d70cd
Robert A. Hill <robert@decentlabs.com>
2022-11-09 10:37:31 -0800

    add security groups

--- terraform/production/ec2.tf
+++ terraform/production/ec2.tf
@@ -8,8 +8,93 @@ module "ec2_instance" {
   instance_type          = local.instance_type
   key_name               = "console"
   monitoring             = true
-  vpc_security_group_ids = module.vpc.default_security_group_id
+  vpc_security_group_ids = [
+    aws_security_group.allow_http.id,
+    aws_security_group.allow_tls.id,
+    aws_security_group.allow_ssh
+  ]
   subnet_id              = element(module.vpc.private_subnets, 0)
 
   tags = local.tags
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [module.vpc.cidr_blocks]
+    ipv6_cidr_blocks = [module.vpc.ipv6_cidr_blocks]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group" "allow_https" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [module.vpc.cidr_blocks]
+    ipv6_cidr_blocks = [module.vpc.ipv6_cidr_blocks]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [module.vpc.cidr_blocks]
+    ipv6_cidr_blocks = [module.vpc.ipv6_cidr_blocks]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
