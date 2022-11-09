7dd2517825c44665d0edbdb73e5e739a100ecf91
Robert A. Hill <robert@decentlabs.com>
2022-11-09 10:22:24 -0800

    cleaner way of selecting subnet

--- terraform/production/ec2.tf
+++ terraform/production/ec2.tf
@@ -9,7 +9,7 @@ module "ec2_instance" {
   key_name               = "console"
   monitoring             = true
   vpc_security_group_ids = module.vpc.default_security_group_id
-  subnet_id              = module.vpc.public_subnets[1]
+  subnet_id              = element(module.vpc.private_subnets, 0)
 
   tags = local.tags
 }
