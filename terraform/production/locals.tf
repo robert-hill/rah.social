e0b2ff4939d5066c81f662706f935055626a14df
Robert A. Hill <robert@decentlabs.com>
2022-11-09 16:41:32 -0800

    Upgrade instance type

--- terraform/production/locals.tf
+++ terraform/production/locals.tf
@@ -1,7 +1,7 @@
 locals {
   name          = "mastodon-server-production"
   region        = "us-west-2"
-  instance_type = "t3a.micro"
+  instance_type = "t3a.medium"
 
   tags = {
     Terraform   = "true"
