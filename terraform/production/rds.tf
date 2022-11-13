506af7393ff3f533c028baa5d715b5a352f0d927
Robert A. Hill <robert@decentlabs.com>
2022-11-12 20:57:56 -0800

    Missed a prefix

--- terraform/production/rds.tf
+++ terraform/production/rds.tf
@@ -22,7 +22,7 @@ module "db" {
 
   multi_az               = true
   db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.db_security_group_id]
+  vpc_security_group_ids = [module.db_security_group.security_group_id]
 
   maintenance_window              = "Mon:00:00-Mon:03:00"
   backup_window                   = "03:00-06:00"
