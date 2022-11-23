72c647123f34a3394f3e918aaa49cb15d9f667ef
Robert A. Hill <robert@decentlabs.com>
2022-11-22 18:37:36 -0800

    Add mail domain

--- terraform/production/route53.tf
+++ terraform/production/route53.tf
@@ -7,3 +7,22 @@ resource "aws_route53_record" "apex" {
   ]
   zone_id = data.aws_route53_zone.this.zone_id
 }
+
+# Mail Records
+
+
+
+resource "aws_route53_zone" "mail" {
+  name = "mail.rah.social"
+}
+
+resource "aws_route53_record" "mail_delegation" {
+  zone_id = data.aws_route53_zone.this
+  name    = "mail.rah.social"
+  type    = "NS"
+  ttl     = local.fast_ttl
+  records = [
+    aws_route53_zone.mail.name_servers
+    ]
+}
+
